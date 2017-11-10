997efe539115650172a782f5a8483c710fe768dd
Kazuhito Yamazawa <rynqs@Kazuhito-no-MBP.aircard>
2017-11-11 02:27:14 +0900

    update node function template

--- functions/template/node/function/handler.js
+++ functions/template/node/function/handler.js
@@ -1,5 +1,12 @@
 "use strict"
 
+const Web3 = require("web3");
+const web3 = new Web3(new Web3.providers.HttpProvider(`http://${process.env.eth_host}:8545`));
+const querystring = require('querystring');
+
 module.exports = (context, callback) => {
-    callback(undefined, {status: "done"});
+    const body = querystring.parse(context);
+    web3.eth.getAccounts((err, result) => {
+        callback(undefined, result);
+    });    
 }
